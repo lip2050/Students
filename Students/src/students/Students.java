fd05bebdc2a040ca1e5ff6b525ddb4ebc01cb21a
lip2050 <lip2050@users.noreply.github.com>
2020-09-18 21:28:38 -0400

    Uploading all the students files.

---
+++ Students/src/students/Students.java
@@ -0,0 +1,21 @@
+package students;
+
+public class Students {
+	
+	String name;
+	int age;
+	
+	
+	public Students(String Name) {
+		
+		this.name = Name;
+		
+	}
+	
+	public Students(int Age) {
+		
+		this.age = Age;
+		
+	}
+
+}
---
+++ Students/src/students/StudentsInformation.java
@@ -0,0 +1,14 @@
+package students;
+
+public class StudentsInformation {
+	
+	public static void main (String []args) {
+		Students sdt = new Students("Candice");
+		Students sdt1 = new Students(28);
+		System.out.println("The Student, " + sdt.name + "'s age information is " + sdt1.age);
+		
+		
+		
+	}
+
+}
